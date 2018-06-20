b4219232b0b24f22d8947997b37b56f4ce3adabe
174high <466243346@qq.com>
2018-06-20 20:19:42 +0800

    add net test 1 for irc

---
+++ build_from_zero-net/net-test-1-IRC/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string.h>
+#include <db_cxx.h>
+#include <db.h>
+#include <QtDebug>
+#include <sys/stat.h>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include "uint256.h"
+#include "util.h"
+#include "serialize.h"
+#include "net.h"
+
+using std::cout;
+using std::endl;
+using std::cerr;
+
+using namespace boost;
+
+
+int main (int argc, char *argv[])
+{
+    int ret; 
+
+
+
+
+    return ret; 
+}
+
+
+
+
+
+
+
+
+
