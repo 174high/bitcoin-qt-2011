01ce71666703231f82b9d5a1d85e001bd77a4835
174high <466243346@qq.com>
2018-06-21 20:40:15 +0800

    add revutil in net test 1 irc and can get data

--- build_from_zero-net/net-test-1-IRC/main.cpp
+++ build_from_zero-net/net-test-1-IRC/main.cpp
@@ -14,8 +14,124 @@
 using std::cout;
 using std::endl;
 using std::cerr;
+using std::string ;
+using std::vector; 
 
 using namespace boost;
+
+static bool Send(SOCKET hSocket, const char* pszSend)
+{
+    if (strstr(pszSend, "PONG") != pszSend)
+        printf("IRC SENDING: %s\n", pszSend);
+    const char* psz = pszSend;
+    const char* pszEnd = psz + strlen(psz);
+    while (psz < pszEnd)
+    {
+        int ret = send(hSocket, psz, pszEnd - psz, MSG_NOSIGNAL);
+        if (ret < 0)
+            return false;
+        psz += ret;
+    }
+    return true;
+}
+
+
+bool RecvLine(SOCKET hSocket, string& strLine)
+{
+    strLine = "";
+    loop
+    {
+        char c;
+        int nBytes = recv(hSocket, &c, 1, 0);
+        if (nBytes > 0)
+        {
+            if (c == '\n')
+                continue;
+            if (c == '\r')
+                return true;
+            strLine += c;
+            if (strLine.size() >= 9000)
+                return true;
+        }
+        else if (nBytes <= 0)
+        {
+            if (fShutdown)
+                return false;
+            if (nBytes < 0)
+            {
+                int nErr = WSAGetLastError();
+                if (nErr == WSAEMSGSIZE)
+                    continue;
+                if (nErr == WSAEWOULDBLOCK || nErr == WSAEINTR || nErr == WSAEINPROGRESS)
+                {
+                    Sleep(10);
+                    continue;
+                }
+            }
+            if (!strLine.empty())
+                return true;
+            if (nBytes == 0)
+            {
+                // socket closed
+                printf("IRC socket closed\n");
+                return false;
+            }
+            else
+            {
+                // socket error
+                int nErr = WSAGetLastError();
+                printf("IRC recv failed: %d\n", nErr);
+                return false;
+            }
+        }
+    }
+}
+
+
+bool RecvLineIRC(SOCKET hSocket, string& strLine)
+{
+    loop
+    {
+        bool fRet = RecvLine(hSocket, strLine);
+        if (fRet)
+        {
+            if (fShutdown)
+                return false;
+            vector<string> vWords;
+            ParseString(strLine, ' ', vWords);
+            if (vWords.size() >= 1 && vWords[0] == "PING")
+            {
+                strLine[1] = 'O';
+                strLine += '\r';
+                Send(hSocket, strLine.c_str());
+                continue;
+            }
+        }
+        return fRet;
+    }
+}
+
+
+
+int RecvUntil(SOCKET hSocket, const char* psz1, const char* psz2=NULL, const char* psz3=NULL, const char* psz4=NULL)
+{
+    loop
+    {
+        string strLine;
+        strLine.reserve(10000);
+        if (!RecvLineIRC(hSocket, strLine))
+            return 0;
+        std::cout<<"IRC="<<strLine;
+        if (psz1 && strLine.find(psz1) != -1)
+            return 1;
+        if (psz2 && strLine.find(psz2) != -1)
+            return 2;
+        if (psz3 && strLine.find(psz3) != -1)
+            return 3;
+        if (psz4 && strLine.find(psz4) != -1)
+            return 4;
+    }
+}
 
 
 bool Wait(int nSeconds)
@@ -66,7 +182,7 @@ int main (int argc, char *argv[])
             else
                 return 0;
     }
-/*
+
     if (!RecvUntil(hSocket, "Found your hostname", "using your IP address instead", "Couldn't look up your hostname", "ignoring hostname"))
     {
             closesocket(hSocket);
@@ -75,11 +191,11 @@ int main (int argc, char *argv[])
             if (Wait(nErrorWait += 60))
                 continue;
             else
-                return;
+                return 0;
     }
 
 
-*/
+
 
     }
 
